9fc5556bca2e7b8386d2a72f29be1d0c7de9d129
N1ceboat <yuyun0124@gmail.com>
2019-05-16 01:03:28 +0900

    EscapeRoom_Prototype : 플레이어 이동

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Animator animator;
+
+    private Rigidbody2D rb2D;
+    bool coll;
+
+    Vector2 MoveVelocity;
+    public float Speed = 5f;
+    void Start() {
+        coll = false;
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        MoveVelocity = moveInput.normalized * Speed;
+    }
+
+    void FixedUpdate() {
+        rb2D.MovePosition(rb2D.position + MoveVelocity * Time.fixedDeltaTime);
+    }
+}
