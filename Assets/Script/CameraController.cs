f17df6c396eed2412f853856a566e58319998ad8
N1ceboat <yuyun0124@gmail.com>
2019-05-16 01:15:00 +0900

    EscapeRoom_Prototype : 카메라 고정 횡스크롤

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+public class CameraController : MonoBehaviour {
+    private Vector2 velocity;
+    private float smoothTimeX;
+    private float smoothTimeY;
+
+    public GameObject player;
+
+    public bool bounds;
+    public Vector3 minCameraPos;
+    public Vector3 maxCameraPos;
+
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+
+    void FixedUpdate() {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = 3f;
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+
+    }
+}
