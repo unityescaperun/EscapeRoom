5aa63f7fb603875ed5116276319e5ec5ecf985f1
Seongjun_Lee_Bit <yuhancam1@gmail.com>
2019-05-26 14:26:30 +0900

    병합용 수정

---
+++ Assets/Script/DialogueTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public static DialogueTrigger instance = null;
+
+    private void Start() {
+        instance = this;
+    }
+
+    public void TriggerDialogue() {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+}
