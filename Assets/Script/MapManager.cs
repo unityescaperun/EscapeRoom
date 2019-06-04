2d3f7517986c8d9b69245150f31413d72ab01d09
N1ceboat <yuyun0124@gmail.com>
2019-06-04 22:38:59 +0900

    Stage1/2/3 버그 수정 // Develop->fixed로 pull request 필요

--- Assets/Script/MapManager.cs
+++ Assets/Script/MapManager.cs
@@ -8,6 +8,8 @@ public class MapManager : MonoBehaviour {
     int rows = 20;
     int columns2 = 5;
     int rows2 = 45;
+    int columns3 = 11;
+    int rows3 = 29;
 
     bool DoorOpen = false;
 
@@ -53,7 +55,7 @@ public class MapManager : MonoBehaviour {
     public GameObject Chair1;
     public GameObject Chair2;
     public GameObject Table;
-    
+
 
     public GameObject Stair_Up;
     public GameObject Stair_Down;
@@ -80,61 +82,78 @@ public class MapManager : MonoBehaviour {
         if (GameManager.stageLevel == 1) {
             Debug.Log("월드1");
             for (int x = -1; x < rows + 1; x++) {
-                for(int y = -1; y < columns + 1; y++) {
+                for (int y = -1; y < columns + 1; y++) {
                     GameObject toInstantiate = wall;
-                    if(x == -1 || x == rows || y == -1 || y == columns || y == 3)
+                    if (x == -1 || x == rows || y == -1 || y == columns || y == 3)
                         toInstantiate = outerWall;
 
                     GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
 
-                    instance.transform.SetParent(boardHolder); 
+                    instance.transform.SetParent(boardHolder);
                 }
             }
         }
 
         // 스테이지 2
-        else if(GameManager.stageLevel == 2) {
+        else if (GameManager.stageLevel == 2) {
             Debug.Log("월드2");
             for (int x = -1; x < rows2 + 1; x++) {
-                for(int y = -1; y < columns2 + 1; y++) {
+                for (int y = -1; y < columns2 + 1; y++) {
                     GameObject toInstantiate = wall;
                     if (x > 31 && x < rows2)
                         toInstantiate = Garbage_Wall;
-                    else if (x == 16)
-                    {
+                    else if (x == 16) {
                         if (y > 0 && y < columns2)
                             toInstantiate = Restaurant_Window_Left1;
                         else if (y == 0)
                             toInstantiate = Restaurant_Window_Left2;
                     }
-                    else if (x == 28)
-                    {
+                    else if (x == 28) {
                         if (y > 0 && y < columns2)
                             toInstantiate = Restaurant_Window_Right1;
                         else if (y == 0)
                             toInstantiate = Restaurant_Window_Right2;
                     }
-                    else if (x > 16 && x < 28)
-                    {
+                    else if (x > 16 && x < 28) {
                         if (y > 0 && y < columns2)
                             toInstantiate = Restaurant_Window_Middle;
                         else if (y == 0)
                             toInstantiate = Restaurant_Window_Bottom;
                     }
 
-                    if(x == -1 || x == 13 || x == 15 || x == 29 || x == 31 || x == rows2)
+                    if (x == -1 || x == 13 || x == 15 || x == 29 || x == 31 || x == rows2)
                         toInstantiate = outerWall;
-                    else if(x == 14 || x == 30)
+                    else if (x == 14 || x == 30)
                         continue;
                     else
-                        if(y == -1 || y == columns2)
-                            toInstantiate = outerWall;
+                        if (y == -1 || y == columns2)
+                        toInstantiate = outerWall;
 
                     GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
 
                     instance.transform.SetParent(boardHolder);
                 }
-            }          
+            }
+        }
+
+        // 스테이지 3
+        else if (GameManager.stageLevel == 3) {
+            Debug.Log("월드3");
+            for (int x = -1; x < rows3 + 1; x++) {
+                for (int y = -1; y < columns3 + 1; y++) {
+                    GameObject toInsatantiate = wall;
+                    if (x == -1 || x == 13 || x == 15 || x == rows3)
+                        toInsatantiate = outerWall;
+                    else if (x == 14 || x == 30)
+                        continue;
+                    else if (y == -1 || y == 5 || y == columns3)
+                        toInsatantiate = outerWall;
+
+                    GameObject instance = Instantiate(toInsatantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+
+                    instance.transform.SetParent(boardHolder);
+                }
+            }
         }
     }
 
@@ -159,7 +178,7 @@ public class MapManager : MonoBehaviour {
             Instantiate(Window1, new Vector3(16, columns - 2, 0f), Quaternion.identity);
             Instantiate(Fireplace, new Vector3(16, columns - 7, 0f), Quaternion.identity);
         }
-        else if(GameManager.stageLevel == 2) {
+        else if (GameManager.stageLevel == 2) {
             Instantiate(Door1_1, new Vector3(12, 0, 0f), Quaternion.identity);
             Instantiate(Door1_2, new Vector3(16, 0, 0f), Quaternion.identity);
             Instantiate(Door2_1, new Vector3(28, 0, 0f), Quaternion.identity);
@@ -187,7 +206,7 @@ public class MapManager : MonoBehaviour {
             Instantiate(Chair1, new Vector3(23, -0.05f, 0f), Quaternion.identity);
             Instantiate(Table, new Vector3(25, -0.15f, 0f), Quaternion.identity);
             Instantiate(Chair2, new Vector3(27, -0.05f, 0f), Quaternion.identity);
-            
+
             Instantiate(exit, new Vector3(rows2 - 1, 0, 0f), Quaternion.identity);
         }
         else if (GameManager.stageLevel == 3) {
