61b18ea3107ebac5823603e2be5a0f514f1303d9
N1ceboat <yuyun0124@gmail.com>
2019-06-17 19:17:58 +0900

    모든 씬 Menu 버튼 추가

---
+++ Assets/Script/ReturnMenuButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ReturnMenuButton : MonoBehaviour {
+    public void OnClickExit() {
+        GameManager.stageLevel = 1;
+        SceneManager.LoadScene("0");
+    }
+
+}
